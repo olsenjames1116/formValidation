452d92442d6ce4b04fc685000aa7cbd935992beb
James Olsen <olsenjames1116@gmail.com>
2023-01-24 16:32:21 -0700

    Event listeners for validity on submit and changes
    Event listeners were added to input elements for whenever a change
    occurs so they can be validated. An event listener was added to the
    submit button to check the validity of all inputs.

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const email = document.querySelector('input#email');
 const password = document.querySelector('input#password');
 const confirmPassword = document.querySelector('input#confirmPassword');
+const country = document.querySelector('select#country');
 const zip = document.querySelector('input#zip');
+const submitButton = document.querySelector('form>div:last-child>button:first-child');
 const clearButton = document.querySelector('form>div:last-child>button:last-child');
 
 
@@ -12,5 +14,49 @@ function clearInput() {
     zip.value = '';
 }
 
+function showEmailError() {
+    console.log('email error');
+}
+
+function showPasswordError () {
+    console.log('password error');
+}
+
+function showZipError() {
+    console.log('zip error');
+}
+
+email.addEventListener('change', () => {
+});
+
+password.addEventListener('change', () => {
+    console.log('password');
+});
+
+confirmPassword.addEventListener('change', () => {
+    console.log('confirm password');
+});
+
+zip.addEventListener('change', () => {
+    console.log('zip');
+});
+
+submitButton.addEventListener('click', () => {
+    if (email.checkValidity() && password.checkValidity() && confirmPassword.checkValidity() && zip.checkValidity()) {
+        alert(`Form accepted!\nEmail: ${email.value}\nPassword: ${password.value}\nCountry: ${country.value}\nZip: ${zip.value}`);
+        clearInput();
+    }
+    else{
+        if (!email.checkValidity()) {
+            showEmailError();
+        }
+        if (!password.checkValidity() || !password.checkValidity()) {
+            showPasswordError();
+        }
+        if (!zip.checkValidity()) {
+            showZipError();
+        }
+    }
+});
 
 clearButton.addEventListener('click', clearInput);
